dc2b285556021e6f0b42d848dbb4873e28119221
dhodcz2 <dhodcz2@uic.edu>
2020-09-20 17:15:08 -0500

    Added bench file

--- main.py
+++ main.py
@@ -4,11 +4,14 @@ import logging
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
-    parser.add_argument('bench', type=str, default='circuit.bench', help='input bench file')
+    parser.add_argument('-b', '--bench', type=str, default='circuit.bench', help='input bench file')
     parser.add_argument('-v', '--verbose', type=bool, default=True, help='verbose simulator')
+    parser.add_argument('-t', '--testvec', type=str, default=None, help='test vector')
     args = parser.parse_args()
 
     simulator = circuit_simulator.CircuitSimulator(args)
-    while(simulator.prompt()):
-        simulator.simulate()
+    simulator.prompt()
+    simulator.simulate()
+    # while simulator.prompt():
+    #     simulator.simulate()
     print("Finished -- bye!")
